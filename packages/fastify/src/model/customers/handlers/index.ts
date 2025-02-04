eca6f289c0dc5aff6aa5735026f0281479347799
Ramesh <ramesh@dzangolab.com>
2025-02-04 17:20:46 +0545

    feat: add enpoints to get and update user's current account

--- packages/fastify/src/model/customers/handlers/index.ts
+++ packages/fastify/src/model/customers/handlers/index.ts
@@ -2,14 +2,18 @@ import create from "./create";
 import remove from "./delete";
 import getById from "./getById";
 import list from "./list";
+import myAccount from "./myAccount";
 import myAccounts from "./myAccounts";
 import update from "./update";
+import updateMyAccount from "./updateMyAccount";
 
 export default {
   create,
   delete: remove,
   getById,
   list,
+  myAccount,
   myAccounts,
   update,
+  updateMyAccount,
 };
