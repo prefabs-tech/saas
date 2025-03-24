ecc2a98c6eec1dbec3dfb110fd86685b4ddb28ef
Suvash Vishowkarma <suvash@dzangolab.com>
2025-03-24 19:53:49 +0545

    feat(my-account): update accounts page

--- packages/react/src/components/my-accounts/Account.tsx
+++ packages/react/src/components/my-accounts/Account.tsx
@@ -1,6 +1,8 @@
 import { useTranslation } from "@dzangolab/react-i18n";
 import { Button, Tag } from "@dzangolab/react-ui";
+import { useNavigate } from "react-router-dom";
 
+import { DEFAULT_PATHS } from "@/constants";
 import { Account as AccountType } from "@/types";
 
 type Properties = {
@@ -12,23 +14,36 @@ type Properties = {
 
 export const Account = ({ account, active, loading, onSwitch }: Properties) => {
   const { t } = useTranslation("accounts");
+  const navigate = useNavigate();
 
   return (
     <div className={`account${active ? " highlight" : ""}`}>
       <div className="header">
         {account.name}
-        {active ? (
-          <Tag label={t("account.active")}></Tag>
-        ) : (
+        <div className="actions">
+          {active ? (
+            <Tag label={t("account.active")}></Tag>
+          ) : (
+            <Button
+              onClick={onSwitch}
+              loading={loading}
+              iconLeft="pi pi-arrow-right-arrow-left"
+              size="small"
+              variant="outlined"
+              severity="secondary"
+            >
+              {t("account.actions.switch")}
+            </Button>
+          )}
           <Button
-            onClick={onSwitch}
-            loading={loading}
+            onClick={() => {
+              navigate(DEFAULT_PATHS.MY_ACCOUNT.replace(":id", account.id));
+            }}
+            iconLeft="pi pi-cog"
             size="small"
-            variant="outlined"
-          >
-            {t("account.actions.switch")}
-          </Button>
-        )}
+            title={t("account.actions.view")}
+          ></Button>
+        </div>
       </div>
     </div>
   );
