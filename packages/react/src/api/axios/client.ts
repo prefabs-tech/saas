dc67d4f6aa29437a4668f8ca7951086f6ecbc782
Suvash Vishowkarma <suvash@dzangolab.com>
2025-03-25 14:08:16 +0545

    feat(my-account): add x-account-id header for all requests

--- packages/react/src/api/axios/client.ts
+++ packages/react/src/api/axios/client.ts
@@ -1,12 +1,15 @@
 import axios from "axios";
 
 export const client = (baseURL: string) => {
+  const accountId = sessionStorage.getItem("x-account-id");
+
   return axios.create({
     baseURL: baseURL,
     headers: {
       post: {
         "Content-Type": "application/json",
       },
+      "x-account-id": accountId || "",
     },
   });
 };
