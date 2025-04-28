427091c5d4cc69cc1ce8d8740b2d1a9a1bfa002b
premsgr77 <prem@dzangolab.com>
2025-04-28 22:52:54 +0545

    feat: update translations

--- packages/vue/src/views/Accounts/Index.vue
+++ packages/vue/src/views/Accounts/Index.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
-    <h1>{{ $t("accounts.title") }}</h1>
+    <h1>{{ t("accounts.title") }}</h1>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useI18n } from "@dzangolab/vue3-i18n";
+
+import { useTranslations } from "../../index";
+
+const messages = useTranslations();
+
+const { t } = useI18n({ messages });
+</script>
