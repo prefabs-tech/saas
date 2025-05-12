529be2dd99a8006f356193c87f1919cf276feb59
premsgr77 <prem@dzangolab.com>
2025-05-12 13:40:19 +0545

    feat: update view page

--- packages/vue/src/views/Customers/View.vue
+++ packages/vue/src/views/Customers/View.vue
@@ -1,7 +1,51 @@
 <template>
-  <div>
-    <h1>Account View TODO</h1>
-  </div>
+  <Page :title="account.name">
+    <TabbedPanel>
+      <div :title="t('customers.view.info')">
+        <div>
+          <span>{{ account.name }}</span>
+        </div>
+      </div>
+      <div :title="t('customers.view.users')">
+        <div>
+          <span>Users</span>
+        </div>
+      </div>
+    </TabbedPanel>
+  </Page>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useConfig } from "@dzangolab/vue3-config";
+import { useI18n } from "@dzangolab/vue3-i18n";
+import { TabbedPanel } from "@dzangolab/vue3-ui";
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
+
+import { useTranslations } from "../../index";
+import useAccountsStore from "../../stores/accounts";
+
+import type { Account } from "../../types/account";
+import type { AppConfig } from "@dzangolab/vue3-config";
+
+const route = useRoute();
+const accountId = route.params.id as string;
+
+const messages = useTranslations();
+const { t } = useI18n({ messages });
+const config = useConfig() as AppConfig;
+
+const accountsStore = useAccountsStore();
+const { getAccount } = accountsStore;
+
+const account = ref({} as Account);
+
+onMounted(async () => {
+  await prepareComponent();
+});
+
+async function prepareComponent() {
+  const response = await getAccount(accountId, config.apiBaseUrl);
+  account.value = response;
+}
+</script>
