5a5c9531defbd1c13c9bd52a9bd0bca5462827a9
premsgr77 <prem@dzangolab.com>
2025-05-16 15:28:36 +0545

    feat: update store for invitations

---
+++ packages/vue/src/api/invitation.ts
@@ -0,0 +1,145 @@
+import client from "./axios";
+import type {
+  AccountInvitation,
+  AccountInvitationCreateInput,
+  GetAccountInvitationsResponse,
+  AddAccountInvitationResponse,
+  DeleteAccountInvitationResponse,
+  ResendAccountInvitationResponse,
+  RevokeAccountInvitationResponse,
+  GetInvitationResponse,
+  AcceptInvitationResponse,
+} from "../types/accountInvitation";
+
+export const addInvitation = async (
+  accountId: string,
+  data: AccountInvitationCreateInput,
+  apiBaseUrl: string
+): Promise<AddAccountInvitationResponse> => {
+  const response = await client(apiBaseUrl).post(
+    `/accounts/${accountId}/invitations`,
+    data,
+    {
+      withCredentials: true,
+    }
+  );
+
+  return response.data;
+};
+
+export const deleteInvitation = async (
+  accountId: string,
+  id: number,
+  apiBaseUrl: string
+): Promise<DeleteAccountInvitationResponse> => {
+  const response = await client(apiBaseUrl).delete(
+    `/accounts/${accountId}/invitations/${id}`,
+    {
+      withCredentials: true,
+    }
+  );
+
+  return response.data;
+};
+
+export const getInvitation = async (
+  accountId: string,
+  id: number,
+  apiBaseUrl: string
+): Promise<GetInvitationResponse> => {
+  const response = await client(apiBaseUrl).get(
+    `/accounts/${accountId}/invitations/${id}`,
+    {
+      withCredentials: true,
+    }
+  );
+
+  return response.data;
+};
+
+export const getInvitations = async (
+  accountId: string,
+  apiBaseUrl: string,
+  params?: object
+): Promise<GetAccountInvitationsResponse> => {
+  const response = await client(apiBaseUrl).get(
+    `/accounts/${accountId}/invitations`,
+    {
+      params,
+      withCredentials: true,
+    }
+  );
+
+  return response.data;
+};
+
+export const joinInvitation = async (
+  token: string,
+  accountId: string | null,
+  apiBaseUrl: string
+): Promise<AccountInvitation> => {
+  const response = await client(apiBaseUrl).post(
+    `/accounts/${accountId}/invitations/join/${token}`,
+    null,
+    {
+      withCredentials: true,
+    }
+  );
+
+  return response.data;
+};
+
+export const resendInvitation = async (
+  accountId: string,
+  id: number,
+  apiBaseUrl: string
+): Promise<ResendAccountInvitationResponse> => {
+  const response = await client(apiBaseUrl).post(
+    `/accounts/${accountId}/invitations/${id}/resend`,
+    null,
+    {
+      withCredentials: true,
+    }
+  );
+
+  return response.data;
+};
+
+export const revokeInvitation = async (
+  accountId: string,
+  id: number,
+  apiBaseUrl: string
+): Promise<RevokeAccountInvitationResponse> => {
+  const response = await client(apiBaseUrl).post(
+    `/accounts/${accountId}/invitations/${id}/revoke`,
+    null,
+    {
+      withCredentials: true,
+    }
+  );
+
+  return response.data;
+};
+
+export const signupInvitation = async (
+  token: string,
+  data: {
+    email: string;
+    password: string;
+    givenName?: string;
+    middleNames?: string;
+    surname?: string;
+  },
+  accountId: string | null | undefined,
+  apiBaseUrl: string
+): Promise<AcceptInvitationResponse> => {
+  const url = accountId
+    ? `/accounts/${accountId}/invitations/token/${token}`
+    : `/invitations/token/${token}`;
+
+  const response = await client(apiBaseUrl).post(url, data, {
+    withCredentials: false,
+  });
+
+  return response.data;
+};
