098b92ee3188509692ad17784d22e8c11fc8c695
Ramesh <ramesh@dzangolab.com>
2025-05-26 15:01:39 +0545

    feat: prevent deleted user to login or verify session

--- packages/fastify/src/supertokens/recipes/session/verifySession.ts
+++ packages/fastify/src/supertokens/recipes/session/verifySession.ts
@@ -16,6 +16,16 @@ const verifySession = (
     if (originalResponse) {
       const user = input.userContext._default.request.request.user;
 
+      if (user?.deletedAt) {
+        await originalResponse.revokeSession();
+
+        throw {
+          name: "SESSION_VERIFICATION_FAILED",
+          message: "user not found",
+          statusCode: 401,
+        } as FastifyError;
+      }
+
       if (user?.disabled) {
         await originalResponse.revokeSession();
 
