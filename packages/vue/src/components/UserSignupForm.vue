b8ef9c30fa8ec163a0d5c340ae4a12e9f77c9fc9
ks-rohan <rohan@dzangolab.com>
2025-08-05 18:00:57 +0545

    feat: update form

--- packages/vue/src/components/UserSignupForm.vue
+++ packages/vue/src/components/UserSignupForm.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="user-signup-form">
-    <Form @submit="onSubmit">
-      <Input
+    <Form :validation-schema="validationSchema" @submit="onSubmit">
+      <Email
+        v-model="formData.email"
+        :disabled="!!email"
+        :label="t('account.signup.fields.email.label')"
+        :placeholder="t('account.signup.fields.email.placeholder')"
+      />
+      <!-- <Input
         v-model="formData.email"
         :label="t('account.signup.fields.email.label')"
         :placeholder="t('account.signup.fields.email.placeholder')"
@@ -9,27 +15,38 @@
         name="email"
         type="email"
         :disabled="!!email"
+      /> -->
+
+      <Password
+        v-model="formData.password"
+        :label="t('account.signup.fields.password.label')"
+        name="password"
       />
 
-      <Input
+      <!-- <Input
         v-model="formData.password"
         :label="t('account.signup.fields.password.label')"
         :placeholder="t('account.signup.fields.password.placeholder')"
         :schema="passwordSchema"
         name="password"
         type="password"
+      /> -->
+
+      <Password
+        :label="t('account.signup.fields.confirmPassword')"
+        name="confirmation"
       />
 
-      <Input
+      <!-- <Input
         v-model="formData.confirmPassword"
         :label="t('account.signup.fields.confirmPassword')"
         :placeholder="t('account.signup.fields.confirmPassword')"
         :schema="confirmPasswordSchema"
         name="confirmPassword"
         type="password"
-      />
+      /> -->
 
-      <div class="terms-checkbox">
+      <!-- <div class="terms-checkbox">
         <input
           id="termsAndConditions"
           v-model="formData.termsAndConditions"
@@ -39,9 +56,43 @@
         <label for="termsAndConditions">
           {{ t("account.signup.termsAndConditions") }}
         </label>
+      </div> -->
+
+      <template v-if="termsAndConditionsConfig?.display">
+        <TermsAndConditions
+          :has-checkbox="!!termsAndConditionsConfig?.showCheckbox"
+          :route="termsAndConditionsConfig?.route"
+          @update:check="disableButton = !$event"
+        >
+          <component
+            :is="customTermsAndCondition"
+            v-if="!!customTermsAndCondition"
+          />
+        </TermsAndConditions>
+      </template>
+
+      <!-- <TermsAndConditions
+        :has-checkbox="!!termsAndConditionsConfig?.showCheckbox"
+        @update:check="disableButton = !$event"
+      >
+        <component
+          :is="customTermsAndCondition"
+          v-if="!!customTermsAndCondition"
+        />
+      </TermsAndConditions> -->
+
+      <div class="actions">
+        <LoadingButton
+          :disabled="
+            disableButton &&
+            termsAndConditionsConfig?.display &&
+            termsAndConditionsConfig?.showCheckbox
+          "
+          :label="t('account.signup.actions.signup')"
+        />
       </div>
 
-      <ButtonElement
+      <!-- <ButtonElement
         type="submit"
         class="submit-button"
         :label="t('account.signup.actions.signup')"
@@ -49,23 +100,27 @@
         @click="onSubmit"
       >
         {{ t("account.signup.actions.signup") }}
-      </ButtonElement>
+      </ButtonElement> -->
     </Form>
   </div>
 </template>
 
 <script setup lang="ts">
-import { Form, Input } from "@prefabs.tech/vue3-form";
+import { useConfig } from "@prefabs.tech/vue3-config";
+import {
+  Email,
+  emailSchema,
+  Password,
+  passwordSchema,
+} from "@prefabs.tech/vue3-form";
 import { useI18n } from "@prefabs.tech/vue3-i18n";
-import { ButtonElement } from "@prefabs.tech/vue3-ui";
-import { computed, ref, watch } from "vue";
+import { LoadingButton } from "@prefabs.tech/vue3-ui";
+import { TermsAndConditions } from "@prefabs.tech/vue3-user";
+import { toFormValidator } from "@vee-validate/zod";
+import { inject, ref, watch } from "vue";
+import { z } from "zod";
 
 import { useTranslations } from "../index";
-import {
-  createEmailSchema,
-  createPasswordSchema,
-  createConfirmPasswordSchema,
-} from "../validations/userValidation";
 
 import type { UserSignupData } from "../types/user";
 
@@ -73,6 +128,15 @@ export interface UserSignupFormProperties {
   email?: string;
   loading?: boolean;
 }
+
+const config = useConfig();
+
+const customTermsAndCondition = inject("dzangolabVueUserTerms");
+
+const disableButton = ref<boolean>(true);
+
+const termsAndConditionsConfig =
+  config.user?.features?.signUp?.termsAndConditions;
 
 const props = withDefaults(defineProps<UserSignupFormProperties>(), {
   email: "",
@@ -103,11 +167,41 @@ watch(
   }
 );
 
-// Form validation schemas
-const emailSchema = createEmailSchema();
-const passwordSchema = createPasswordSchema();
-const confirmPasswordSchema = computed(() =>
-  createConfirmPasswordSchema(formData.value.password)
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+let fieldSchema: Record<string, any> = {
+  email: emailSchema(
+    {
+      invalid: t("user.signup.form.email.errors.invalid"),
+      required: t("user.signup.form.email.errors.required"),
+    },
+    config?.user?.options?.email
+  ),
+  password: passwordSchema(
+    {
+      required: t("user.signup.form.password.errors.required"),
+      weak: t("user.signup.form.password.errors.weak"),
+    },
+    config?.user?.options?.password
+  ),
+  confirmation: passwordSchema(
+    {
+      required: t("user.signup.form.password.errors.required"),
+      weak: t("user.signup.form.password.errors.weak"),
+    },
+    { minLength: 0 }
+  ),
+};
+
+const validationSchema = toFormValidator(
+  z.object(fieldSchema).refine(
+    (data) => {
+      return data.password === data.confirmation;
+    },
+    {
+      message: t("user.signup.form.confirmation.errors.match"),
+      path: ["confirmation"],
+    }
+  )
 );
 
 function onSubmit() {
--- packages/vue/src/types/plugin.ts
+++ packages/vue/src/types/plugin.ts
@@ -1,3 +1,5 @@
+import type { AppConfig } from "@prefabs.tech/vue3-config";
+import type { DzangolabVueUserConfig } from "@prefabs.tech/vue3-user";
 import type { LocaleMessages, VueMessageType } from "@prefabs.tech/vue3-i18n";
 import type { Pinia } from "pinia";
 import type { Component } from "vue";
@@ -23,12 +25,19 @@ export type SaasEventHandlers = {
 };
 
 interface SaasVuePluginOptions {
+  config: AppConfig;
   accountTabs?: (defaultTabs: Tab[]) => Tab[];
   notification?: (message: EventMessage) => void;
   pinia: Pinia;
   router: Router;
   saasConfig: SaasConfig;
   translations?: LocaleMessages<VueMessageType>;
+}
+
+declare module "@prefabs.tech/vue3-config" {
+  export interface AppConfig {
+    user?: DzangolabVueUserConfig;
+  }
 }
 
 export type { SaasVuePluginOptions };
