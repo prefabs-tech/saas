9845854c30ae688f02d587558e64aca09d8b6a0d
ks-rohan <rohan@dzangolab.com>
2025-09-17 14:17:33 +0545

    feat(vue-main-app): implement injection of additional tabs

--- packages/vue/src/views/Accounts/AccountSettings.vue
+++ packages/vue/src/views/Accounts/AccountSettings.vue
@@ -1,5 +1,6 @@
 <template>
   <Page
+    v-if="activeAccount?.id"
     :loading="loading"
     :title="activeAccount?.name"
     :title-tag="
@@ -9,29 +10,95 @@
     "
     class="account-show account-settings"
   >
-    <AccountShow v-if="activeAccount && !loading" :account="activeAccount" />
+    <TabView
+      v-if="processedTabs.length > 0 && visibleTabs.length > 0"
+      id="account-settings-tabs"
+      :tabs="processedTabs"
+      :visible-tabs="visibleTabs"
+      :active-key="activeTab"
+      @update:active-key="activeTab = $event"
+    >
+      <div v-for="tab in processedTabs" :key="tab.key">
+        <component
+          :is="getComponentByTabKey(tab.key)"
+          :account="activeAccount"
+        />
+      </div>
+    </TabView>
   </Page>
 </template>
 
 <script setup lang="ts">
 import { useI18n } from "@prefabs.tech/vue3-i18n";
-import { Page } from "@prefabs.tech/vue3-ui";
-import { ref, onMounted, computed } from "vue";
+import { Page, TabView } from "@prefabs.tech/vue3-ui";
+import { ref, onMounted, computed, inject } from "vue";
 
 import { useTranslations } from "../../index";
+import AccountDetails from "./_components/AccountDetails.vue";
 import { useMyAccountsStore } from "../../stores/myAccounts";
-import AccountShow from "../Accounts/_components/AccountShow.vue";
+import Invitations from "../Invitations/Index.vue";
+import Users from "../Users/Index.vue";
+
+import type { Component } from "vue";
 
 const messages = useTranslations();
 const { t } = useI18n({ messages });
 const myAccountsStore = useMyAccountsStore();
 
+const __saasAccountTabs = Symbol.for("saas.accountTabs");
+
 const activeAccount = computed(() => myAccountsStore.activeAccount);
 const loading = ref(true);
+const activeTab = ref("info");
+
+const defaultTabList = [
+  {
+    component: AccountDetails,
+    key: "info",
+    label: t("account.view.info"),
+  },
+  {
+    component: Users,
+    key: "users",
+    label: t("account.view.users"),
+  },
+  {
+    component: Invitations,
+    key: "invitations",
+    label: t("account.view.invitations"),
+  },
+];
+
+const tabConfig = inject<(() => typeof defaultTabList) | typeof defaultTabList>(
+  __saasAccountTabs,
+  defaultTabList
+);
+
+const processedTabs = computed(() => {
+  const additionalTabs =
+    typeof tabConfig === "function"
+      ? tabConfig()
+      : Array.isArray(tabConfig)
+        ? tabConfig
+        : [];
+
+  const result = [...defaultTabList, ...additionalTabs];
+
+  return result;
+});
+
+const visibleTabs = computed(() => processedTabs.value.map((tab) => tab.key));
 
 onMounted(async () => {
   await prepareComponent();
 });
+
+function getComponentByTabKey(tabKey: string): Component {
+  return (
+    processedTabs.value.find((tab) => tab.key === tabKey)?.component ||
+    AccountDetails
+  );
+}
 
 async function prepareComponent() {
   loading.value = true;
--- packages/vue/src/views/Accounts/View.vue
+++ packages/vue/src/views/Accounts/View.vue
@@ -66,10 +66,14 @@ const defaultTabList = [
   },
 ];
 
+console.log("__saasAccountTabs", __saasAccountTabs);
+
 const tabConfig = inject<(() => typeof defaultTabList) | typeof defaultTabList>(
   __saasAccountTabs,
   defaultTabList
 );
+
+console.log("tabConfig", tabConfig);
 
 const processedTabs = computed(() => {
   const additionalTabs =
@@ -78,10 +82,17 @@ const processedTabs = computed(() => {
       : Array.isArray(tabConfig)
         ? tabConfig
         : [];
+
+  console.log("additionalTabs", additionalTabs);
+
   return [...defaultTabList, ...additionalTabs];
 });
 
+console.log("processedTabs", processedTabs.value);
+
 const visibleTabs = computed(() => processedTabs.value.map((tab) => tab.key));
+
+console.log("visibleTabs", visibleTabs.value);
 
 onMounted(async () => {
   await prepareComponent();
